b327ed905071c49b2bace64df3ac22c131b7a262
rybalidoy <rybalidoy@gmail.com>
2024-10-27 00:13:21 +0800

    add automation for git

--- start.bat
+++ start.bat
@@ -1,4 +1,27 @@
 @echo off
 title Hanami Minecraft Server
-java -Xmx2048M -Xms1024M -jar server.jar nogui
+
+REM Pull the latest changes from the main branch
+git pull origin main
+echo Pull latest server data
+
+REM Start the Minecraft Server
+start /wait java -Xmx2048M -Xms1024M -jar server.jar nogui
+
+REM Check if server stoppped normally
+
+IF ERRORLEVEL 1 (
+  echo Server stopped unexpectedly
+) ELSE (
+  echo Server stopped successfully
+  REM Navigate to server directory
+  
+  git add .
+  REM Commit changes
+  git commit -m "Automated commit: Minecraft server stopped and saved changes"
+
+  REM Push the changes to the remote repository
+  git push origin main
+)
+
 pause
