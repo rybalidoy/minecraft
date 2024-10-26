a0f40655b954909a7ddeaa20d31b597af0943e63
rybalidoy <rybalidoy@gmail.com>
2024-10-27 00:34:06 +0800

    Automated commit: Minecraft server stopped and saved changes on 2024-10-27 00:34

--- start.bat
+++ start.bat
@@ -6,7 +6,7 @@ echo Pulling latest server data
 git pull origin main
 
 REM Start the Minecraft server in a new window
-start "Minecraft Server" cmd /c java -Xmx1024M -Xms1024M -jar server.jar nogui
+start "Minecraft Server" cmd /c java -Xmx2048M -Xms1024M -jar server.jar nogui
 
 REM Wait for the Minecraft server to stop
 echo Waiting for the Minecraft server to stop...
@@ -22,14 +22,18 @@ set dt=%dt:~0,4%-%dt:~4,2%-%dt:~6,2% %dt:~8,2%:%dt:~10,2%
 REM Check if the server stopped normally or was interrupted
 echo Server stopped successfully
 
-REM Stage changes for commit
+REM Check if there are changes to commit
 git add .
+git diff-index --quiet HEAD --
+if %errorlevel% neq 0 (
+    REM Commit the changes with the current date and time
+    git commit -m "Automated commit: Minecraft server stopped and saved changes on %dt%"
 
-REM Commit the changes with the current date and time
-git commit -m "Automated commit: Minecraft server stopped and saved changes on %dt%"
-
-REM Push the changes to the remote repository
-echo Pushing updates to GitHub
-git push origin main
+    REM Push the changes to the remote repository
+    echo Pushing updates to GitHub
+    git push origin main
+) else (
+    echo No changes to commit
+)
 
 pause
